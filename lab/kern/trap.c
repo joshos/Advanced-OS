78d9519afe45cab306ba9e5d4544c8f2c7023e63
joshos <advrtos@gmail.com>
2016-10-30 23:46:29 -0400

    Add files via upload

--- lab/kern/trap.c
+++ lab/kern/trap.c
@@ -155,7 +155,7 @@ trap_init_percpu(void)
 	//thiscpu->cpu_ts.ts_ss0 = GD_KD;
 
 	// Initialize the TSS slot of the gdt.
-	gdt[(GD_TSS0 >> 3) + cpunum()] = SEG16(STS_T32A, (uint32_t) (&ts),
+	gdt[(GD_TSS0 >> 3) + cpunum()] = SEG16(STS_T32A, (uint32_t) (&thiscpu->cpu_ts),
 					sizeof(struct Taskstate) - 1, 0);
 	gdt[(GD_TSS0 >> 3) + cpunum()].sd_s = 0;
 
